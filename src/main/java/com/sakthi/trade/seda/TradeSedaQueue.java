9d0d26327c1a97871c4723ab4dee8fe155654851
hasvanth <sakthirhce@gmail.com>
2023-02-23 22:55:43 +0530

    update

---
+++ src/main/java/com/sakthi/trade/seda/TradeSedaQueue.java
@@ -0,0 +1,42 @@
+package com.sakthi.trade.seda;
+
+import com.sakthi.trade.seda.TelegramSedaProcessor;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.seda.SedaComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class TradeSedaQueue {
+
+    CamelContext camelContext=new DefaultCamelContext();
+
+    @Autowired
+    TelegramSedaProcessor telegramSedaProcessor;
+    @PostConstruct
+    @Qualifier("camelContextRoute")
+    public CamelContext init() throws Exception {
+        SedaComponent sedaComponent = new SedaComponent();
+        sedaComponent.setQueueSize(3);
+        camelContext.addComponent("telegramQueue", camelContext.getComponent("seda"));
+        camelContext.addComponent("placeQueue", camelContext.getComponent("seda"));
+        camelContext.addRoutes(new RouteBuilder() {
+            public void configure() throws Exception {
+                from("seda:telegramQueue")
+                        .process(telegramSedaProcessor)
+                        .end();
+            }
+        });
+        camelContext.start();
+        return camelContext;
+    }
+
+    public void sendTelemgramSeda(String message){
+        camelContext.createProducerTemplate().sendBody("seda:telegramQueue", message);
+    }
+}
